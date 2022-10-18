5ee274697dd0fd32b2f72f87b70ecc8ce67bf389
allisteraniceto <allisteraniceto@gmail.com>
2022-10-18 08:53:23 -0700

    edited template, constructors and added compare function

--- AVL.h
+++ AVL.h
@@ -3,21 +3,35 @@
 using namespace std;
 
 //implement as templated class (can use multiple data types)
-template <class T>
+template <typename T>
 class AVLnode{
 private:
     T data;
     AVLnode<T>* left; //pointer to left child
     AVLnode<T>* right; //pointer to right child
+    int leftHeight;
+    int rightHeight;
+    bool checkBalance;
 public:
     AVLnode(){ //default constructor
-        data = T(); //template syntax
-        right = null; //child pointers to null
+        data=null;
+        right=null; //child pointer to null
+        left=null;
+    }
+    AVLnode(typename T){ //overloaded constructor
+        data = T;
+        right = null; 
         left = null;
+    }
+    T getData(){
+        return data;
+    }
+    int getHeight(){
+        return;
     }
 };
 
-template <class T>
+template <typename T>
 class AVLtree{
 private:
     AVLnode<T>* root; //pointer to root
@@ -27,45 +41,45 @@ public:
         root=null;
         int=0;
     }
-    AVLnode insert(AVLnode<T>root, T data){
+    AVLnode insert(T data){
         if (root==null){ //base case
             root->T;
         }
-        if (data > root->T){ //if data > parent
+        if (data > root->T.getData()){ //if data > parent
             root->right=insert(root, data); //recursive implementation
         }
-        else if (data < root->T){ //if data < parent
+        else if (data < root->T.getData()){ //if data < parent
             root->left=insert(root, data);
         }
         return root; 
     }
     //4 cases
-    AVLtree llRotate(AVLnode* parent){ //LEFT LEFT ROTATE (right rotate only)
-        AVLnode *newP; //new parent node
+    AVLtree llRotate(AVLnode<T>* parent){ //LEFT LEFT ROTATE (right rotate only)
+        AVLnode<T> *newP; //new parent node
         newP=parent->left; //left child becomes parent
         parent->left=newP->right; //left child points to newP right child (null)
         newP->right=parent; //newp right child points to parent node
         cout << "Performed left left rotation" << endl;
         return newP; //return sub tree
     }
-    AVLtree lrRotate(AVLnode* parent){ //LEFT RIGHT ROTATE (right rotate, then rrRotate)
-        AVLNode* tempParent; //for first rotation
+    AVLtree lrRotate(AVLnode<T>* parent){ //LEFT RIGHT ROTATE (right rotate, then rrRotate)
+        AVLNode<T>* tempParent; //for first rotation
         tempParent=parent->right; //right child becomes temp new parent used for right rotate
         tempParent->right=parent->right->left; // right rotation on temp new parent
         tempParent->left=null; //tempParent left child set to null, now in right right case;
         cout << "Performed left right rotation" << endl;
         return rrRotate(parent); //now left rotate around parent
     }
-    AVLtree rrRotate(AVLnode* parent){ //RIGHT RIGHT ROTATE (left rotate only)
-        AVLnode *newP; //new parent node
+    AVLtree rrRotate(AVLnode<T>* parent){ //RIGHT RIGHT ROTATE (left rotate only)
+        AVLnode<T> *newP; //new parent node
         newP=parent->right; //right child becomes parent
         parent->right=newP->left; //right child points to newP left (null)
         newP->left=parent;
         cout << "Performed right right rotation" << endl;
         return newP; 
     }
-    AVLtree rlRotate(AVLnode* parent){ //RIGHT LEFT ROTATE (left rotate, the llRotate)
-        AVLnode* tempParent;
+    AVLtree rlRotate(AVLnode<T>* parent){ //RIGHT LEFT ROTATE (left rotate, the llRotate)
+        AVLnode<T>* tempParent;
         tempParent=parent->right; //temp new parent set to left child
         tempParent->left=parent->left->right; //right rotate around temp parent
         tempParent->right=null; //set temp parent right pointer to null, now left left case
--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 #include "AVL.h"
 using namespace std;
 
+/* 
+TASKS:
+
+-make compare function and make a function pointer as a parameter of AVLtree constructor
+-insert deletion;
+
+*/
+
+template<typename T>
+int compareNode(T left, T right){
+    if (left < right)
+        return -1;
+    else if (left == right)
+        return 0;
+    else if (left > right)
+        return 1;
+}
+
 int main(){
+    AVLtree<int> tree;
+    tree.insert(2);
+    tree.insert(392);
+    tree.insert(239);
+    tree.insert(32);
+    tree.insert(57009);
     return 0;
 }
